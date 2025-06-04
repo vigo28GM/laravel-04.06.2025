cdf86cceb2438213c558596f7559563559e87325
Vigo28 <gamingvigo06@gmail.com>
2025-06-04 10:15:18 +0300

    fixed show

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,6 @@ Route::get('/', function () {
 Route::get('/books', [BookController::class, 'index'])->name('books.index');
 Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
 Route::post('/books/store', [BookController::class, 'store'])->name('books.store');
-Route::get('/books/{id}', [BookController::class, 'show'])->name('books.show');
+Route::get('/books/{id}/show', [BookController::class, 'show'])->name('books.show');
 Route::get('/books/{id}/edit', [BookController::class, 'edit'])->name('books.show');
 Route::put('/books/{id}/update', [BookController::class, 'update'])->name('books.index');
